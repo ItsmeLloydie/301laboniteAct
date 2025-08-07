5542aa5f7aeacbddca3d322d2df0e8c070de4670
ItsmeLloydie <168395482+ItsmeLloydie@users.noreply.github.com>
2025-08-07 13:02:55 +0000

    eto na

---
+++ act3.dart
@@ -0,0 +1,18 @@
+void main( ) {
+  double celsius = 25.0;
+  double fahrenheit = 100.0;
+
+  print('===Temperature Converter===')
+
+  double ConverterToF = celsiusToFahrenheit(celsius);
+  print('${celsius}') = ${ConverterToF.toStringAsFixed(1)}F);
+
+  double ConverterToC = fahrenheitToCelsius(fahrenheit);
+  print('${fahrenheit}') = ${ConverterToC.toStringAsFixed(1)}F);
+
+
+}
+
+double celsiusToFahrenheit(double celsius) {
+  return
+}
